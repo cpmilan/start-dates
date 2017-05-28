12b62cc67ed5b1b73ec1b7a7a301921a2ff38287
cpmilan <cpmilan@gmail.com>
2017-05-28 18:54:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ def makeWebhookResult(req):
 
     #dates = {"France":'July 3rd', "UK":'July 3rd or 31st'}
 
-    speech = "External candidates can start on " + month + start_date + " in " location + "."
+    speech = "External candidates can start on " + month + start_date + " in " + location + "."
 
     print("Response:")
     print(speech)
