e689f2711cd7961ac8987c3b062674bd96745aca
cpmilan <cpmilan@gmail.com>
2017-05-28 17:42:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,20 +12,20 @@ from flask import make_response
 #NOTE: the below code makes the whole webhook crash...can't understand why -_- #
 
 import gspread
-#from oauth2client.service_account import ServiceAccountCredentials
+from oauth2client.service_account import ServiceAccountCredentials
 
 #gspread credentials
 
-#json_key = 'gspread-test.json'
-#scope = ['https://spreadsheets.google.com/feeds']
+json_key = 'gspread-test.json'
+scope = ['https://spreadsheets.google.com/feeds']
 
-#credentials = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 
-#gc = gspread.authorize(credentials)
+gc = gspread.authorize(credentials)
 
-#spr = gc.open_by_key("1_afG4TmSYG6v1hJxcIWc5hyXMMZbFxXzL9-0856DXmU")
+spr = gc.open_by_key("1_afG4TmSYG6v1hJxcIWc5hyXMMZbFxXzL9-0856DXmU")
 
-#wks = spr.worksheet("Sheet1")
+wks = spr.worksheet("Sheet1")
 
 # = = = = = = = #
 
